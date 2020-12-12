8fda8a6e32643c6d70357e3710b9af4df40d68d0
JoakimRudbeck <sbu470@edu.sollentuna.se>
2020-12-12 20:13:31 +0100

    Rearranging

--- src/kapitel01_Grunder/Övningar.java
+++ src/kapitel01_Grunder/Övningar.java
@@ -2,9 +2,11 @@ package kapitel01_Grunder;
 
 public class Övningar {
 
-
-    public static void main(String[] args) {
-        övning1_2();
+    public static void main(String[] args) { // Detta är main-metoden. Här börjar programmet köra om kompileringen går igenom.
+        System.out.println("Kör övning 1.2:");
+        övning1_2(); // Anrop till metoden övning1_2
+        System.out.println("Kör övning 1.3:");
+        övning1_3();
     }
 
     static void övning1_2() {
@@ -13,6 +15,10 @@ public class Övningar {
         System.out.println("C++");
         System.out.println("Java");
         System.out.println("Visual Basic");
+    }
+
+    static void övning1_3() {
+        // TODO: Skriv ut texten To be or not to be ...
     }
 
 
