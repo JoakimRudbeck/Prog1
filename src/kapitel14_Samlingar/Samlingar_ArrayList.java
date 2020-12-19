d16e46be3bc1e21ad25fe4f728c8ea87a240ed7d
JoakimRudbeck <sbu470@edu.sollentuna.se>
2020-12-19 19:27:08 +0100

    Update ArrayList

--- src/kapitel14_Samlingar/Samlingar_ArrayList.java
+++ src/kapitel14_Samlingar/Samlingar_ArrayList.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Samlingar_ArrayList {
 
     public static void main(String[] args) {
-        //arrayListExempel();
+        arrayListExempel();
         arrayListMedSträngarExempel();
     }
 
