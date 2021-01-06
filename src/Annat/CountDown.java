bf80ec28f4a570a2a4639560ce38cc8f99953f95
JoakimRudbeck <sbu470@edu.sollentuna.se>
2021-01-06 13:12:12 +0100

    Möblering

---
+++ src/Annat/CountDown.java
@@ -0,0 +1,41 @@
+package Annat;
+
+import static java.lang.Thread.sleep;
+
+public class CountDown {
+
+    private static final int MINUTER_KVAR = 10;
+    private static final int TALBAS = 2; // funkar med 2, 8, 10, 16
+    private static String MEDDELANDE = "Tid kvar av rasten: ";
+    private static String SLUT_MEDDELANDE = "Nu är rasten slut";
+
+    public static void main(String[] args) throws InterruptedException {
+        countDown(MINUTER_KVAR, TALBAS, 1000);
+    }
+
+    private static void countDown(long minuterKvar, int bas, int millis) throws InterruptedException {
+        for(long i = 60*minuterKvar; i >= 0; i--){
+            System.out.print(MEDDELANDE.toUpperCase());
+            switch(bas){
+                case 2:
+                    System.out.println(Long.toBinaryString(i));
+                    break;
+                case 8:
+                    System.out.println(Long.toOctalString(i));
+                    break;
+                case 10:
+                    System.out.println(i);
+                    break;
+                case 16:
+                    System.out.println(Long.toHexString(i));
+                    break;
+                default:
+                    System.out.println(i);
+
+            }
+            sleep(millis);
+        }
+        System.out.printf("#### %s ####", SLUT_MEDDELANDE.toUpperCase());
+    }
+
+}
