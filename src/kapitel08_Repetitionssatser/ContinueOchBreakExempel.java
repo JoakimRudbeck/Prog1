e0901902d5985e273deb2d2127dd61a8c7a1f925
JoakimRudbeck <sbu470@edu.sollentuna.se>
2021-03-15 15:31:44 +0100

    Adding more comments to ContinueOchBreakExempel.java

--- src/kapitel08_Repetitionssatser/ContinueOchBreakExempel.java
+++ src/kapitel08_Repetitionssatser/ContinueOchBreakExempel.java
@@ -11,28 +11,26 @@ public class ContinueOchBreakExempel {
 
         // Exempel med continue:
         // Skriv ut följande talföljd:  2 4 6 8 12 14 16 18 22 24 26
-
-        for(int i = 2; i <= 26; i+=2){
-            if(i % 10 == 0){ // Om i jämnt delbar med 10
-                continue;
+        for(int i = 2; i <= 26; i+=2){      // Loopen börjar på 2, ökar med 2 och slutar vid 26.
+            if(i % 10 == 0){                // Om i jämnt delbar med 10 (då i är 10 och 20)
+                continue;                   // Hoppa vidare till nästa varv
             }
-            System.out.print(i + " ");
+            System.out.print(i + " ");      // Skriv ut värdet på i.
         }
 
         // Exempel med break:
-        // Skriv ut följande talföljd: 2 4 6 8 12 14 16 18 22 24 26
-
+        // Skriv ut följande talföljd (samma som ovan): 2 4 6 8 12 14 16 18 22 24 26
         int räknare = 2;
-        while(true){
-            if(räknare > 26){
-                break;  // Avbryter hela loopen
+        while(true){                        // Oändlig loop
+            if(räknare > 26){               // Om vi gått förbi 26
+                break;                      // Avbryter hela loopen
             }
-            else if(räknare % 10 == 0){
-                räknare += 2;
-                continue;
+            else if(räknare % 10 == 0){     // Om räknaren är jämnt delbar med 10 (för 10 och 20)
+                räknare += 2;               // Öka räknaren med 2
+                continue;                   // Hoppa till nytt varv i loopen
             }
             System.out.print(räknare + " ");
-            räknare+=2;
+            räknare+=2;                     // Öka räknaren med 2
         }
 
     }
